9471834a27272fb144806b6c085c6ea7b14ad36e
your1154 <142734414+your1154@users.noreply.github.com>
2023-08-21 03:31:12 +0900

    Delete graduation directory

--- graduation/backgroud.js
+++
@@ -1,9 +0,0 @@
-const images = ["sea_1.jpg","forest_2.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
